0096f8b50a817ac6f090a69446cf29bbf4eae44b
Guled <somnibyte@gmail.com>
2015-10-14 22:22:56 -0500

    Initial Commit

---
+++ SPlayer Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SPlayer Example
+//
+//  Created by Guled on 10/14/15.
+//  Copyright © 2015 Guled. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
